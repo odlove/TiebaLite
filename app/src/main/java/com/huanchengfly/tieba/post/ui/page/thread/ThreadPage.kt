97043bb3b74ae07584784f7abb7f909409acb921
健忘症 <50003754+zzc10086@users.noreply.github.com>
2025-06-20 09:52:28 +0800

    fix:点赞数未对齐

--- app/src/main/java/com/huanchengfly/tieba/post/ui/page/thread/ThreadPage.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/page/thread/ThreadPage.kt
@@ -266,7 +266,7 @@ private fun BottomBarAgreeBtn(
     ) {
         Row(
             modifier = Modifier.align(Alignment.CenterVertically),
-            verticalAlignment = Alignment.Top
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(
                 imageVector = if (hasAgreed) Icons.Rounded.Favorite else Icons.Rounded.FavoriteBorder,
